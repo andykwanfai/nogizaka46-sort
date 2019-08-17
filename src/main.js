978121d623a62591d48f519a9d350d42730b76e2
Andy <andykwanfai@gmail.com>
2019-08-17 21:08:49 +0800

    upgrade vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import storeConfig from './store/index'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
@@ -13,5 +15,6 @@ const store = new Vuex.Store(storeConfig)
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
