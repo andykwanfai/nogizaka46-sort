de591432f4565d19ab74d6d1d8b1ad1f9bf030f2
Andy <andykwanfai@gmail.com>
2019-09-16 01:28:21 +0800

    Add share and copy

--- src/members.js
+++ src/members.js
@@ -1,231 +1,231 @@
 export const members = [
   {
-    'name': '秋元真夏',
+    'name': '秋元 真夏',
     'generation': 1,
     'img': 'akimotomanatsu_prof'
   },
   {
-    'name': '生田絵梨花',
+    'name': '生田 絵梨花',
     'generation': 1,
     'img': 'ikutaerika_prof'
   },
   {
-    'name': '井上小百合',
+    'name': '井上 小百合',
     'generation': 1,
     'img': 'inouesayuri_prof'
   },
   {
-    'name': '齋藤飛鳥',
+    'name': '齋藤 飛鳥',
     'generation': 1,
     'img': 'saitouasuka_prof'
   },
   {
-    'name': '斉藤優里',
+    'name': '斉藤 優里',
     'generation': 1,
     'img': 'saitouyuuri_prof'
   },
   {
-    'name': '桜井玲香',
+    'name': '桜井 玲香',
     'generation': 1,
     'img': 'sakuraireika_prof'
   },
   {
-    'name': '白石麻衣',
+    'name': '白石 麻衣',
     'generation': 1,
     'img': 'shiraishimai_prof'
   },
   {
-    'name': '高山一実',
+    'name': '高山 一実',
     'generation': 1,
     'img': 'takayamakazumi_prof'
   },
   {
-    'name': '中田花奈',
+    'name': '中田 花奈',
     'generation': 1,
     'img': 'nakadakana_prof'
   },
   {
-    'name': '樋口日奈',
+    'name': '樋口 日奈',
     'generation': 1,
     'img': 'higuchihina_prof'
   },
   {
-    'name': '星野みなみ',
+    'name': '星野 みなみ',
     'generation': 1,
     'img': 'hoshinominami_prof'
   },
   {
-    'name': '松村沙友理',
+    'name': '松村 沙友理',
     'generation': 1,
     'img': 'matsumurasayuri_prof'
   },
   {
-    'name': '和田まあや',
+    'name': '和田 まあや',
     'generation': 1,
     'img': 'wadamaaya_prof'
   },
   {
-    'name': '伊藤かりん',
+    'name': '伊藤 かりん',
     'generation': 2,
     'img': 'itoukarin_prof'
   },
   {
-    'name': '伊藤純奈',
+    'name': '伊藤 純奈',
     'generation': 2,
     'img': 'itoujunna_prof'
   },
   {
-    'name': '北野日奈子',
+    'name': '北野 日奈子',
     'generation': 2,
     'img': 'kitanohinako_prof'
   },
   {
-    'name': '佐々木琴子',
+    'name': '佐々木 琴子',
     'generation': 2,
     'img': 'sasakikotoko_prof'
   },
   {
-    'name': '新内眞衣',
+    'name': '新内 眞衣',
     'generation': 2,
     'img': 'shinuchimai_prof'
   },
   {
-    'name': '鈴木絢音',
+    'name': '鈴木 絢音',
     'generation': 2,
     'img': 'suzukiayane_prof'
   },
   {
-    'name': '寺田蘭世',
+    'name': '寺田 蘭世',
     'generation': 2,
     'img': 'teradaranze_prof'
   },
   {
-    'name': '堀未央奈',
+    'name': '堀 未央奈',
     'generation': 2,
     'img': 'horimiona_prof'
   },
   {
-    'name': '山崎怜奈',
+    'name': '山崎 怜奈',
     'generation': 2,
     'img': 'yamazakirena_prof'
   },
   {
-    'name': '渡辺みり愛',
+    'name': '渡辺 みり愛',
     'generation': 2,
     'img': 'watanabemiria_prof'
   },
   {
-    'name': '伊藤理々杏',
+    'name': '伊藤 理々杏',
     'generation': 3,
     'img': 'itouriria_prof'
   },
   {
-    'name': '岩本蓮加',
+    'name': '岩本 蓮加',
     'generation': 3,
     'img': 'iwamotorenka_prof'
   },
   {
-    'name': '梅澤美波',
+    'name': '梅澤 美波',
     'generation': 3,
     'img': 'umezawaminami_prof'
   },
   {
-    'name': '大園桃子',
+    'name': '大園 桃子',
     'generation': 3,
     'img': 'oozonomomoko_prof'
   },
   {
-    'name': '久保史緒里',
+    'name': '久保 史緒里',
     'generation': 3,
     'img': 'kuboshiori_prof'
   },
   {
-    'name': '阪口珠美',
+    'name': '阪口 珠美',
     'generation': 3,
     'img': 'sakaguchitamami_prof'
   },
   {
-    'name': '佐藤楓',
+    'name': '佐藤 楓',
     'generation': 3,
     'img': 'satoukaede_prof'
   },
   {
-    'name': '中村麗乃',
+    'name': '中村 麗乃',
     'generation': 3,
     'img': 'nakamurareno_prof'
   },
   {
-    'name': '向井葉月',
+    'name': '向井 葉月',
     'generation': 3,
     'img': 'mukaihazuki_prof'
   },
   {
-    'name': '山下美月',
+    'name': '山下 美月',
     'generation': 3,
     'img': 'yamashitamizuki_prof'
   },
   {
-    'name': '吉田綾乃クリスティー',
+    'name': '吉田 綾乃クリスティー',
     'generation': 3,
     'img': 'yoshidaayanochristie_prof'
   },
   {
-    'name': '与田祐希',
+    'name': '与田 祐希',
     'generation': 3,
     'img': 'yodayuuki_prof'
   },
   {
-    'name': '遠藤さくら',
+    'name': '遠藤 さくら',
     'generation': 4,
     'img': 'endousakura_prof'
   },
   {
-    'name': '賀喜遥香',
+    'name': '賀喜 遥香',
     'generation': 4,
     'img': 'kakiharuka_prof'
   },
   {
-    'name': '掛橋沙耶香',
+    'name': '掛橋 沙耶香',
     'generation': 4,
     'img': 'kakehashisayaka_prof'
   },
   {
-    'name': '金川紗耶',
+    'name': '金川 紗耶',
     'generation': 4,
     'img': 'kanagawasaya_prof'
   },
   {
-    'name': '北川悠理',
+    'name': '北川 悠理',
     'generation': 4,
     'img': 'kitagawayuri_prof'
   },
   {
-    'name': '柴田柚菜',
+    'name': '柴田 柚菜',
     'generation': 4,
     'img': 'shibatayuna_prof'
   },
   {
-    'name': '清宮レイ',
+    'name': '清宮 レイ',
     'generation': 4,
     'img': 'seimiyarei_prof'
   },
   {
-    'name': '田村真佑',
+    'name': '田村 真佑',
     'generation': 4,
     'img': 'tamuramayu_prof'
   },
   {
-    'name': '筒井あやめ',
+    'name': '筒井 あやめ',
     'generation': 4,
     'img': 'tsutsuiayame_prof'
   },
   {
-    'name': '早川聖来',
+    'name': '早川 聖来',
     'generation': 4,
     'img': 'hayakawaseira_prof'
   },
   {
-    'name': '矢久保美緒',
+    'name': '矢久保 美緒',
     'generation': 4,
     'img': 'yakubomio_prof'
   },
--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout align-center justify-center row fill-height>
+  <v-layout justify-center row fill-height>
     <v-simple-table id="result">
       <thead>
         <tr>
@@ -14,6 +14,32 @@
         </tr>
       </tbody>
     </v-simple-table>
+    <v-btn class="ma-2" color="blue" dark @click.stop="dialog = true">
+      <v-icon dark>mdi-share-variant</v-icon>
+    </v-btn>
+    <v-dialog v-model="dialog" max-width="500">
+      <v-card>
+        <v-card-title>
+          <div class="flex-grow-1"></div>
+          <v-btn text icon large>
+            <v-icon @click="dialog = false">close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-textarea id="result-string" outlined no-resize v-bind:value="resultString()"></v-textarea>
+        </v-card-text>
+        <v-divider></v-divider>
+        <div id="copy-label">コピー：</div>
+        <v-card-actions>
+          <v-btn fab dark color="grey" @click="range=0;copy()">
+            <!-- <v-icon>mdi-content-copy</v-icon> -->
+            ALL
+          </v-btn>
+          <v-btn fab dark color="grey" @click="range=5;copy()">TOP 5</v-btn>
+          <v-btn fab dark color="grey" @click="range=10;copy()">TOP 10</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
@@ -21,16 +47,44 @@
 import { mapState } from "vuex";
 export default {
   props: ["sorted"],
-  data: () => ({}),
+  data: () => ({ dialog: false, range: 0 }),
   created: function() {
     if (!this.$props.sorted) {
       this.$router.push({ name: "home" });
     }
+    // this.$store.dispatch("init");
   },
-  methods: {},
-  computed: mapState({
-    result: state => state.result
-  })
+  methods: {
+    resultString: function() {
+      let string = "";
+      const result = this.$store.state.result;
+      let length;
+      if (this.range === 0) {
+        length = result.length;
+      } else {
+        length = this.range;
+      }
+
+      for (let i = 0; i < length; i++) {
+        string += `${i + 1} ${result[i].name}\n`;
+      }
+      return string;
+    },
+    copy: async function() {
+      await setTimeout(function() {}, 100);
+      const copyText = document.getElementById("result-string");
+      if (copyText) {
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      result: state => state.result
+    })
+  }
 };
 </script>
 
@@ -40,5 +94,17 @@ export default {
   td {
     font-size: 20px;
   }
+}
+.v-card__actions {
+  padding-left: 24px;
+  padding-bottom: 16px;
+}
+.v-card__text {
+  padding-bottom: 0px;
+}
+
+#copy-label {
+  padding-left: 24px;
+  padding-top: 5px;
 }
 </style>
