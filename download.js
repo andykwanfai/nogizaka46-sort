0ef5f1db19c6ac86d7e95f31e68c9cc25295a80f
andykwanfai <andykwanfai@gmail.com>
2020-03-25 14:39:03 +0900

    change to 25th single profile pic

--- download.js
+++ download.js
@@ -10,7 +10,7 @@ function saveImageToDisk(url, localPath) {
   });
 }
 
-const folder = '24th single'
+const folder = '25th single'
 
 members.map((member) => {
   if (!member.graduation) {
--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card v-ripple="{ center: true }" v-on:click="select" class="ma-1" hover>
-    <v-img v-bind:src="`${publicPath}img/profile%20pic/24th%20single/${img}.jpg`"></v-img>
+    <v-img v-bind:src="`${publicPath}img/profile%20pic/25th%20single/${img}.jpg`"></v-img>
     <v-card-title class="justify-center">
       <b>{{name}}</b>
     </v-card-title>
